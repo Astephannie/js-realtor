0e2fdfc0d82250516d9bb7217c784de5363a3ef3
astephannie <astephannie@gmail.com>
2016-11-20 21:11:28 -0500

    Added smooth scrolling

--- js/js.js
+++ js/js.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
     $('#fullpage').fullpage();
+
+	// Add smooth scrolling to all links
+	$("a").on('click', function(event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+			// Store hash
+			var hash = this.hash;
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function(){
+			window.location.hash = hash;
+			});
+		} // End if
+	});
 });
